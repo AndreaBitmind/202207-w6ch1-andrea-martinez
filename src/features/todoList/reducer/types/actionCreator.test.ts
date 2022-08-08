59c9be6aae755e20d41fecb3d688fdd21a88bbec
Andrea Martinez <andrea.maort@gmail.com>
2022-08-08 22:18:13 +0200

    action creator added and tested

---
+++ src/features/todoList/reducer/types/actionCreator.test.ts
@@ -0,0 +1,35 @@
+import { toDo } from "../../models/models";
+import { loadTodosTaskActionNew } from "../actionCreator";
+
+describe("Given an action creator function", () => {
+  describe("When it's invoked with a list of tasks as a payload and 'loadTodoList' as a type", () => {
+    test("It should return an action with these two properties", () => {
+      const todoListTestPayload: toDo[] = [
+        {
+          id: 1,
+          name: "comprar fruta",
+          done: false,
+        },
+        {
+          id: 2,
+          name: "depilación",
+          done: false,
+        },
+        {
+          id: 3,
+          name: "recoger paquete",
+          done: false,
+        },
+      ];
+
+      const expectedAction = {
+        type: "loadTodoList",
+        payload: todoListTestPayload,
+      };
+
+      const expectedResult = loadTodosTaskActionNew(todoListTestPayload);
+
+      expect(expectedAction).toEqual(expectedResult);
+    });
+  });
+});
